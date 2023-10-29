1d50ea897726d7f9ac6ce2f8a070d292921f704f
Dora Yaroshenko <yar-dora@yandex.ru>
2023-10-29 22:23:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const http = require("http");
 const path = require("path");
-const cors  = require("cors");
+// const cors  = require("cors");
 
 const { routesInit } = require("./routes/configRoutes");
 const { config } = require("./config/secret");
